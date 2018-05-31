33dae84ba5aece7231101f4aacaf043e8e51504f
Victoria Daug <victoria.daug@gmail.com>
2018-06-01 00:01:38 +0100

    make it pretty :nail_care:

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,43 +1,54 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
-import { Link, Redirect} from 'react-router-dom';
-import {fetchStatsData} from '../actions/protected-data';
 import './progress.css'
 
 export class Progress extends React.Component {
 
-  elementIcon(){
-
-    return '🥇';'🥈';'🥉'
+  groupArrays(coll, length) {
+    if (coll.length <= length) return [coll];
+    let list = [];
+    for (let i = 0; i < coll.length; i+=length) {
+      list.push(coll.slice(i, i+length))
+    }
+    return list
   }
 
   render(){
+    let progress = "loading"
+    if (this.props.statsList) {
+      progress = this.props.statsList.map((stat, index) => {
+        const {name, symbol, correct, incorrect } = stat
+        return(
+          <div key={index}>
+            <section className="progressBox">
+              <div className="littleElement">
+                <h1 className="progressElementSymbol">{symbol}</h1>
+                <h2 className="progressElementName">{name}</h2>
+                <p className="correctElement">Correct answers {correct}</p>
+                <p className="incorrectElement">Incorrect answers {incorrect}</p>
+              </div>
+            </section>
 
-      let progress = "loading"
-    if(this.props.statsList){
-     progress = this.props.statsList.map((stat, index) => {
-    const {name, symbol, correct, incorrect } = stat
-
-      return(
-        <div key={index}>
-          <section className="progressBox">
-            <div className="littleElement">
-              <h1 className="progressElementSymbol">{symbol}</h1>
-              <h2 className="progressElementName">{name}</h2>
-            </div>
-            <p className="correctElement">{correct}</p>
-            <p className="incorrectElement">{incorrect}</p>
-          </section>
-
-        </div>
-      )
-    })}
+          </div>
+        )
+      })
+      const groupedList = this.groupArrays(progress, 5);
+      progress = groupedList.map((list, i) => {
+        return (
+          <ul key={i}>
+            {
+              list.map((el, i) => (<li key={i}>{el}</li>) )
+            }
+          </ul>
+        )
+      })
+    }
 
       return (
       <div>
-        <h1 className="progress">Progress</h1>
-        <div>
+        <h1 className="progressHeader">Progress</h1>
+        <div className="Progress" >
         {progress}
         </div>
       </div>
